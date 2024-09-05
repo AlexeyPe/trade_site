46a9e73dd1c6b3673750df8115e1e0e02f1ecf5f
Alexey Pechenckin <pechenckinalexey@gmail.com>
2024-09-05 17:04:00 +0500

    init, ColorModeDarkLight

---
+++ app.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  import {
+    PopoverArrow,
+    PopoverClose,
+    PopoverContent,
+    PopoverPortal,
+    PopoverRoot,
+    PopoverTrigger,
+  } from 'radix-vue'
+  </script>
+  
+<template>
+  <ColorModeDarkLight/>
+</template>
---
+++ components/ColorModeDarkLight.vue
@@ -0,0 +1,29 @@
+<template>
+  <Icon 
+    :name="isDark ? 'carbon:moon' : 'carbon:sun'"
+    @click="isDark = !isDark"
+    class="hover:cursor-pointer"/>
+</template>
+
+<script setup>
+const colorMode = useColorMode()
+const isDark = computed({
+    get () {
+      return colorMode.value === 'dark'
+    },
+    set () {
+      colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+    }
+  })
+</script>
+
+<style>
+body {
+  background-color: #fff;
+  color: rgba(0,0,0,0.8);
+}
+.dark-mode body {
+  background-color: #0c0c0c;
+  color: #ebf4f1;
+}
+</style>
